e2d856fc141a754c4e5801b530bb44b888ea496a
jigneshkumarpatel <jigneshkumarpatel@pranays-mbp-2>
2017-08-19 16:12:08 -0400

    First Commit

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,161 @@
-
+//Pranay Patel
+// imports the random method !! 
+import java.util.Random;
 public class Board {
+	// Initializes variables that will later be used. 
+	private final int size = 3;
+	public int player1;
+	public int player2;
+	public int [][] board = new int [size][size];
+	// creates a constructor. 
+	public void Board(){
+		// assigns values to variables and creates the tic tac toe board.
+		player1 = 1;
+		player2 = 2;
+		for(int row =0; row <board.length; row++){
+			for(int column = 0; column< board[row].length; column++){
+				board[row][column] = 0;
+			}
+		}		
+	}
+	// this is another constructor that is used for mode2. Same thing except it assigns a value
+	// to the middle of the board
+	public void Board(int x1){
+		player1 = 1;
+		player2 = 2;
+		for(int row =0; row <board.length; row++){
+			for(int column = 0; column< board[row].length; column++){
+				board[row][column] = 0;
+			}
+		}	
+		board[1][1] = x1;
+		
+	}
+	// method prints the board. not ever used except for when debugging to see what's going on
+	public void showBoard(){
+		for(int row = 0; row<board.length; row++ ){
+			for(int column = 0; column<board[row].length; column++){
+				System.out.print(board[row][column]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	// assigns a 1 to a random area in the double matrix aka our board. 
+	public void player1move(){
+		Random r = new Random();
+		int x = r.nextInt(size);
+		int y = r.nextInt(size);
+		while(board[x][y] == 1 || board[x][y] ==2){
+			x = r.nextInt(size);
+			y = r.nextInt(size);
+		}
+		board[x][y] = 1;
+		
+	}
+	// assigns a 2 to a random area in the double matrix; this is our player 2 !
+	public void player2move(){
+		Random r = new Random();
+		int x = r.nextInt(size);
+		int y = r.nextInt(size);
+		while(board[x][y] == 1 || board[x][y] ==2){
+			x = r.nextInt(size);
+			y = r.nextInt(size);
+		}
+		board[x][y] = 2;	
+	}
+	// this is a method created specifically for mode3. basically, if there is not already a 
+	// 1 in the middle of the board, player 2 will pick that spot his/her first turn.
+	public boolean mode3(){
+		boolean mode3 = false;
+		if(board[1][1] != 1){
+			board[1][1] = 2;
+			mode3 = true;
+		}
+		return mode3;
+		
+	}
+	// this actually plays the game. checks to see if the board is full nor not. 
+	public boolean playGame(){
+		boolean gameOver = true;
+		for(int row =0; row <board.length; row++){
+			for(int column = 0; column< board[row].length; column++){
+				if(board[row][column] == 0){
+					gameOver = false;
+				}
+			}
+		}		
+		return gameOver;
+	}
+	// statements to see if player one has won this thing or not 
+	public boolean isGameOver(){
+		 if( board [0][0]==board[1][0] && board[1][0] == board[2][0] && (board [0][0]==1 || board [0][0]==2))
+             return true;
+ else if( board [0][1]==board[1][1] && board[1][1] == board[2][1] && (board [0][1]== 1 || board [0][1]==2))
+             return true;
+ else if( board [0][2]==board[1][2] && board[1][2] == board[2][2] && (board [0][2]==1 || board [0][2]==2))
+             return true;
+ else if( board [0][0]==board[0][1] && board[0][1] == board[0][2] && (board [0][0]==1 || board [0][0]==2))
+             return true;
+ else if( board [1][0]==board[1][1] && board[1][1] == board[1][2] && (board [1][0]==1 || board [1][0]==2))
+             return true;
+ else if( board [2][0]==board[2][1] && board[2][1] == board[2][2] && (board [2][0]==1|| board [2][0]==2))
+             return true;
+ else if( board [0][0]==board[1][1] && board[1][1] == board[2][2] && (board [0][0]==1 || board [0][0]==2))
+             return true;
+ else if( board [2][0]==board[1][1] && board[1][1] == board[0][2] && (board [2][0]==1 || board [2][0]==2))
+             return true;
+ else
+             return false;
+		
+		
+	}
+	// checks to see if player 1 won or not. this will be influential in our main method.
+	public boolean checkIfWinner() {
+   if( (board [0][0] == board[1][0] && board[1][0] == board[2][0]) && (board [0][0] == 1)){
+               return true;
+   }
+   else if(( board [0][1]==board[1][1] && board[1][1] == board[2][1]) && (board [0][1]==1)){
+               return true;
+   }
+   else if(( board [0][2]==board[1][2] && board[1][2] == board[2][2]) && (board [0][2]== 1)){
+               return true;
+   }
+        
+   else if((board [0][0]==board[0][1] && board[0][1] == board[0][2]) && (board [0][0]== 1)){
+               return true;
+   }
+   else if((board [1][0]==board[1][1] && board[1][1] == board[1][2]) && (board [1][0]== 1)){
+               return true;
+   }
+   else if((board [2][0]==board[2][1] && board[2][1] == board[2][2]) && (board [2][0]== 1)){
+               return true;
+   }
+   else if(( board [0][0]==board[1][1] && board[1][1] == board[2][2]) && (board [0][0]== 1)){
+               return true;
+   }
+   else if(( board [2][0]==board[1][1] && board[1][1] == board[0][2]) && (board [2][0]== 1))
+               return true;
+   else{
+               return false;
+   }
+}
+	// checks to see if it there is a tie. 
+	public boolean checkIfTie() {
+		if(checkIfWinner() == true){
+			return false;
+		}
+	    for (int i = 0; i < 3; i++){
+	        for (int j=0; j < 3; j++){
+	            if(board [i][j]== 0){
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+	
+	
 
+	
 }
--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,129 @@
-
+// Pranay Patel
 public class Game {
-
-}
+	public static void main(String [] args){
+		// creates an object mode1 for the game.
+		Board mode1 = new Board();
+		// assigns an intial value of 0 to the count of wins, loses, and ties. 
+		int win = 0;
+		int lose= 0;
+		int tie=  0;
+		// mode 1 starts. for loop for the 1000 games. 
+		for(int i = 0; i<1000; i++){
+			mode1.Board();
+			// each game is played as long as play game returns a false. 
+		while(mode1.playGame() == false){
+			// moves a value for player 1
+			mode1.player1move();
+			// checks to see if game is still on
+			if(mode1.isGameOver() == true){
+				break;
+			}
+			// if game is on, then player 2 moves and ends the game if after player 2 the game
+			// ends via a win, lose, or tie. 
+			if(mode1.playGame() == false){
+				mode1.player2move();
+				if(mode1.isGameOver() == true){
+					break;
+				}
+			}
+			}
+		// conditional statements to determine if there was win,lose, or tie and keeps a count.
+		if(mode1.checkIfWinner() == true && mode1.checkIfTie() == false){
+			win++;
+		}
+		else if(mode1.checkIfTie() == true && mode1.checkIfWinner() == false){
+			tie++;
+		}
+		else{
+			lose++;
+		}
+		}
+		// print statement for mode1.
+		System.out.println("For mode 1: " + " win: " + win + " lose: " + lose + " tie: " + tie);
+		// mode 2 starts. resets win lose and tie counts to 0
+		Board mode2 = new Board();
+		 win = 0;
+		 lose= 0;
+		 tie=  0;
+		 // 1000 game for loop.
+		for(int i = 0; i<1000; i++){
+			// instead of normal constructor, another constructor is used.
+			// this will put the first move in the middle always.
+			mode2.Board(1);
+			// player 2 moves after this.
+			mode2.player2move();
+			// conditional statement now throws game into a loop until it ends.
+			// everything else is the same as mode1.
+		while(mode2.playGame() == false){
+			mode2.player1move();
+			if(mode2.isGameOver() == true){
+				break;
+			}
+			if(mode2.playGame() == false){
+				mode2.player2move();
+				if(mode2.isGameOver() == true){
+					break;
+				}
+			}
+			}
+		if(mode2.checkIfWinner() == true && mode2.checkIfTie() == false){
+			win++;
+		}
+		else if(mode2.checkIfTie() == true && mode2.checkIfWinner() == false){
+			tie++;
+		}
+		else{
+			lose++;
+			
+		}
+		}
+		System.out.println("For mode 2: " + " win: " + win + " lose: " + lose + " tie: " + tie);
+		// mode3, the constructors are reset.
+		 win = 0;
+		 lose= 0;
+		 tie=  0;
+		 // creates an object for mode3
+		Board mode3 = new Board();
+		for(int i = 0; i<1000; i++){
+			// moves player one but then if the mode3 method returns a true aka middle space is empty,
+			// player 2 moves there.
+			mode3.Board();
+			mode3.player1move();
+			if(mode3.mode3() == true){
+				mode3.mode3();
+			}
+			// if player one selects the middle space his first turn, player two just moves somewhwhere else
+			else{
+				mode3.player2move();
+			}
+			// while loop starts. everything else is the exact same pretty much 
+		while(mode3.playGame() == false){
+			mode3.player1move();
+			if(mode3.isGameOver() == true){
+				break;
+			}
+			if(mode3.playGame() == false){
+				mode3.player2move();
+				if(mode3.isGameOver() == true){
+					break;
+				}
+			}
+			}
+		if(mode3.checkIfWinner() == true && mode3.checkIfTie() == false){
+			win++;
+			
+		}
+		else if(mode3.checkIfTie() == true && mode3.checkIfWinner() == false){
+			tie++;
+			
+		}
+		else{
+			lose++;
+			
+		}
+		}
+		// prints everything 
+		System.out.println("For mode 3: " + " win: " + win + " lose: " + lose + " tie: " + tie);
+		
+	}
+	}
